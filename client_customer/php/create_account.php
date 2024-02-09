6151af20a7d686fd55dea553155fcaef875e65cf
Crunchies3 <113993166+Crunchies3@users.noreply.github.com>
2024-02-09 17:31:13 +0800

    can now validate inputs and also debug

--- client_customer/php/create_account.php
+++ client_customer/php/create_account.php
@@ -1,7 +1,34 @@
 <?php
 
-include_once './config.php';
+require_once "./config.php";
 
-if (isset($_POST['submit'])) {
-    
+$firstName = $lastName = $emailAddress = $mobileNumber = $password = $confirmPassword = "";
+
+// variables that will hold error messages
+$firstName_err = $lastName_err = $emailAddress_err = $mobileNumber_err = $password_err = $confirmPassword_err = "";
+
+validateInputs();
+
+
+
+function validateInputs()
+{
+    if ($_SERVER["REQUEST_METHOD"] != "POST") {
+    }
+
+    // validate firstname
+    $firstName = trim($_POST["firstName"]);
+
+    if (empty($firstName)) {
+        $firstName_err = "Please enter your first name.";
+    }
+
+    // validate lastname
+    $lastName = trim($_POST["lastName"]);
+
+    if (empty($lastName)) {
+        $firstName_err = "Please enter your last name.";
+    }
+
+    $emailAddress = trim($_POST["lastName"]);
 }
--- client_customer/register_page.php
+++ client_customer/register_page.php
@@ -1,3 +1,80 @@
+<?php
+
+require_once "./php/config.php";
+
+$firstName = $lastName = $emailAddress = $mobileNumber = $password = $confirmPassword = "";
+
+// variables that will hold error messages
+$firstName_err = $lastName_err = $emailAddress_err = $mobileNumber_err = $password_err = $confirmPassword_err = $checkBox_err = "";
+
+validateInputs();
+
+
+
+function validateInputs()
+{
+
+    global $firstName_err, $lastName_err, $emailAddress_err, $mobileNumber_err, $password_err, $confirmPassword_err, $checkBox_err;
+    global $firstName, $lastName, $emailAddress, $mobileNumber, $password, $confirmPassword;
+
+    if ($_SERVER["REQUEST_METHOD"] != "POST") {
+        return;
+    }
+
+    // validate firstname
+    $firstName = trim($_POST["firstName"]);
+
+    if (empty($firstName)) {
+        $firstName_err = "Please enter your first name.";
+    }
+
+    $lastName = trim($_POST["lastName"]);
+
+    if (empty($lastName)) {
+        $lastName_err = "Please enter your last name.";
+    }
+
+    $emailAddress = trim($_POST["email"]);
+
+    if (empty($emailAddress)) {
+        $emailAddress_err = "Please enter your email address.";
+    } else if (!filter_var($emailAddress, FILTER_VALIDATE_EMAIL)) {
+        $emailAddress_err = "Please enter a valid email address";
+    }
+
+    $mobileNumber = trim($_POST["mobile"]);
+
+    if (empty($mobileNumber)) {
+        $mobileNumber_err = "Please enter your mobile number.";
+    } else if (!is_numeric($mobileNumber)) {
+        $mobileNumber_err = "Please enter a valid mobile number.";
+    }
+
+    $password = trim($_POST["password"]);
+
+    if (empty($password)) {
+        $password_err = "Please enter a password.";
+    } else if (strlen($password) < 8) {
+        $password_err = "Please enter a password with atleast 8 characters.";
+    } else if (true) {
+    }
+
+    $confirmPassword = trim($_POST["confirmPassword"]);
+
+    if (empty($confirmPassword)) {
+        $confirmPassword_err = "Please enter a password.";
+    } else if ($confirmPassword != $password) {
+        $confirmPassword_err = "Password does not match.";
+    }
+
+    if (!isset($_POST['checkBox'])) {
+        $checkBox_err = "Please Agree before submitting";
+    }
+}
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -15,40 +92,64 @@
 
         <div class="row rounded-5 p-3 bg-white box-area">
 
-            <div class="col-md-6 d-flex justify-content-center align-items-center flex-column left-box" style="background: #1B75BB;">
+            <div class="col-lg-6 d-flex justify-content-center align-items-center flex-column left-box" style="background: #1B75BB;">
                 <div class="feature-image mb-3">
                     <img src="./images/twin-peaks-logo.png" alt="Twin Peaks" style="width: 250px;">
                 </div>
             </div>
 
-            <div class="col-md-6 right-box">
+            <div class="col-lg-6 right-box">
                 <div class="row align-items-center">
                     <div class="header-text mb-4">
                         <h1>Create Account</h1>
                     </div>
-                    <form class="row g-2" method="post">
+                    <form class="row g-2" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post" novalidate>
                         <div class="col-md-6 mb-2">
-                            <input type="text" class="form-control input-field" placeholder="First name" aria-label="First name">
+                            <input name="firstName" type="text" class="form-control input-field <?php echo (!empty($firstName_err)) ? 'is-invalid' : ''; ?>" placeholder="First name" aria-label="First name" value="<?php echo $firstName; ?>">
+                            <div class="invalid-feedback">
+                                <?php echo $firstName_err; ?>
+                            </div>
                         </div>
                         <div class="col-md-6 mb-2">
-                            <input type="text" class="form-control input-field" placeholder="Last name" aria-label="Last name">
+                            <input name="lastName" type="text" class="form-control input-field <?php echo (!empty($lastName_err)) ? 'is-invalid' : ''; ?>" placeholder="Last name" aria-label="Last name" value="<?php echo $lastName; ?>">
+                            <div class="invalid-feedback">
+                                <?php echo $lastName_err; ?>
+                            </div>
                         </div>
                         <div class="mb-2 col-12">
-                            <input type="email" class="form-control fs-6 input-field" placeholder="Email Adress">
+                            <input name="email" type="email" class="form-control fs-6 input-field <?php echo (!empty($emailAddress_err)) ? 'is-invalid' : ''; ?>" placeholder="Email Adress" value="<?php echo $emailAddress; ?>">
+                            <div class="invalid-feedback">
+                                <?php echo $emailAddress_err; ?>
+                            </div>
                         </div>
                         <div class="mb-2 col-12">
-                            <input type="text" class="form-control fs-6 input-field" placeholder="Mobile Number">
+                            <input name="mobile" type="text" class="form-control fs-6 input-field <?php echo (!empty($mobileNumber_err)) ? 'is-invalid' : ''; ?>" placeholder="Mobile Number" value="<?php echo $mobileNumber; ?>">
+                            <div class="invalid-feedback">
+                                <?php echo $mobileNumber_err; ?>
+                            </div>
                         </div>
                         <div class="mb-2 col-12">
-                            <input type="password" class="form-control fs-6 input-field" placeholder="Password">
+                            <input name="password" type="password" class="form-control fs-6 input-field <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>" placeholder="Password">
+                            <div class="invalid-feedback">
+                                <?php echo $password_err; ?>
+                            </div>
+                        </div>
+                        <div class="mb-2 col-12">
+                            <input name="confirmPassword" type="password" class="form-control fs-6 input-field <?php echo (!empty($confirmPassword_err)) ? 'is-invalid' : ''; ?>" placeholder="Confirm Password">
+                            <div class="invalid-feedback">
+                                <?php echo $confirmPassword_err; ?>
+                            </div>
                         </div>
                         <div class="form-check mb-4">
-                            <input type="checkbox" id="formCheck" class="form-check-input chk-box">
+                            <input name="checkBox" type="checkbox" id="formCheck" class="form-check-input chk-box <?php echo (!empty($checkBox_err)) ? 'is-invalid' : ''; ?>">
                             <label class="lbl-chk">
                                 <small>I agree to the <a href="#">terms of services</a> and <a href="#">privacy</a>
                                     policy
                                 </small>
                             </label>
+                            <div class="invalid-feedback">
+                                <?php echo $checkBox_err; ?>
+                            </div>
                         </div>
                         <div class="input-group mb-3">
                             <button type="submit" name="submit" class="btn btn-lg w-100 fs-6" style="background-color: #1B75BB; color: whitesmoke; font-weight: 600;">Sign
