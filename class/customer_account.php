fc307ed47755dcc5f06bc4df616dd9016bff41e3
Crunchies3 <113993166+Crunchies3@users.noreply.github.com>
2024-02-12 20:50:57 +0800

    gibulagbulag ang file paras classes

---
+++ class/customer_account.php
@@ -0,0 +1,98 @@
+<?php
+
+require_once "./classes/user_account.php";
+
+class CustomerAccount extends UserAccount
+{
+    public $conn;
+
+    function __construct($conn)
+    {
+        $this->conn = $conn;
+    }
+
+    public function login($email, $password)
+    {
+        try {
+
+            $stmt = $this->conn->prepare("SELECT * FROM tbl_customer WHERE email = ?");
+            $stmt->bind_param("s", $email);
+            $stmt->execute();
+            $result = $stmt->get_result();
+
+            if ($result->num_rows > 0) {
+                while ($row = $result->fetch_assoc()) {
+                    $hashedPassword = $row["password"];
+                    if (!password_verify($password, $hashedPassword)) {
+                        return "Incorrect email or password";
+                    } else {
+                        session_start();
+                        $_SESSION["loggedin"] = true;
+                        $_SESSION["customerid"] = $row["customerid"];
+
+                        header("location: ./dashboard.php");
+                    }
+                }
+            } else {
+                return "Incorrect email or password";
+            }
+        } catch (Exception $e) {
+            echo 'Caught exception: ',  $e->getMessage(), "\n";
+        }
+    }
+
+
+    public function register($customerId, $firstName, $lastName, $emailAddress, $mobileNumber, $hashedPassword)
+    {
+        try {
+            $stmt = $this->conn->prepare("INSERT INTO tbl_customer (customerid, firstname, lastname, email, mobilenumber, password) VALUES (?,?,?,?,?,?)");
+            $stmt->bind_param("ssssss", $customerId, $firstName, $lastName, $emailAddress, $mobileNumber, $hashedPassword);
+            $stmt->execute();
+            $stmt->close();
+            header("location: dashboard.php");
+        } catch (Exception $e) {
+            echo 'Caught exception: ',  $e->getMessage(), "\n";
+        }
+        $this->conn->close();
+    }
+
+    public function isIdUnique($customerId)
+    {
+        try {
+            $stmt = $this->conn->prepare("SELECT * FROM tbl_customer WHERE customerid = ?");
+            $stmt->bind_param("s", $customerId);
+
+            $stmt->execute();
+            $result = $stmt->get_result();
+
+            if ($result->num_rows > 0) {
+                $stmt->close();
+                return false;
+            }
+            $stmt->close();
+            return true;
+        } catch (Exception $e) {
+            echo 'Caught exception: ',  $e->getMessage(), "\n";
+        }
+    }
+
+    public function isEmailUnique($email)
+    {
+        try {
+            $stmt = $this->conn->prepare("SELECT * FROM tbl_customer WHERE email = ?");
+            $stmt->bind_param("s", $email);
+
+            $stmt->execute();
+            $result = $stmt->get_result();
+
+            if ($result->num_rows > 0) {
+                $stmt->close();
+                return false;
+            }
+            $stmt->close();
+            return true;
+        } catch (Exception $e) {
+            echo 'Caught exception: ',  $e->getMessage(), "\n";
+        }
+    }
+}
---
+++ class/user_account.php
@@ -0,0 +1,56 @@
+<?php
+
+abstract class UserAccount
+{
+    private $email;
+    private $hashedPassword;
+
+
+    abstract public function login($email, $password);
+    abstract public function register($customerId, $firstName, $lastName, $emailAddress, $mobileNumber, $hashedPassword);
+    abstract public function isIdUnique($id);
+    abstract public function isEmailUnique($email);
+
+    public function validatePassword($password)
+    {
+        if (empty($password)) {
+            return "Please enter a password.";
+        } else if (strlen($password) < 8) {
+            return "Please enter a password with atleast 8 characters.";
+        } elseif (!preg_match("#[0-9]+#", $password)) {
+            return "Your Password Must Contain At Least 1 Number!";
+        } elseif (!preg_match("#[A-Z]+#", $password)) {
+            return "Your Password Must Contain At Least 1 Capital Letter!";
+        } elseif (!preg_match("#[a-z]+#", $password)) {
+            return "Your Password Must Contain At Least 1 Lowercase Letter!";
+        } else {
+            $this->hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+            return "";
+        }
+    }
+
+    public function validateEmail($email)
+    {
+
+        if (empty($email)) {
+            return "Please enter your email address.";
+        } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            return "Please enter a valid email address";
+        } else if (!$this->isEmailUnique($email)) {
+            return "Email already exist";
+        } else {
+            $this->email = $email;
+            return "";
+        }
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function getHashedPassword()
+    {
+        return $this->hashedPassword;
+    }
+}
