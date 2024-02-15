8b8c6c1f8869d64469916e7cc52c3ba0d7a61047
Crunchies3 <113993166+Crunchies3@users.noreply.github.com>
2024-02-15 20:37:39 +0800

    fixed ui. added loading spinners

--- client_admin/reset_password.php
+++ client_admin/reset_password.php
@@ -16,7 +16,7 @@ require_once 'php/reset_password.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="./css/styles.css">
+    <link rel="stylesheet" href="./css/register_page_styles.css">
     <script src="https://kit.fontawesome.com/3a742f337b.js" crossorigin="anonymous"></script> <!-- MATA SA PASSWORd -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <title>Login</title>
@@ -28,7 +28,7 @@ require_once 'php/reset_password.php';
         <div class="row box-area shadow-lg">
             <div class="col-lg-6 d-flex justify-content-center align-items-center flex-column left-box">
                 <div class="feature-image mb-3">
-                    <img src="./images/twinpeaks.png" alt="Twin Peaks" style="width: 250px;" class="mt-4 mb-3">
+                    <img src="./images/twin-peaks-logo.png" alt="Twin Peaks" style="width: 250px;" class="mt-4 mb-3">
                 </div>
             </div>
             <div class="col-lg-6 right-box">
