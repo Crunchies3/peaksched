f8eb7417d15a0856ee14f4ef72286ca4cd0ccf32
jonaldpaner <jonaldpaner@gmail.com>
2024-02-16 22:04:25 +0800

    Fixed the display

---
+++ client_admin/profile_account_page.php
@@ -0,0 +1,213 @@
+<?php
+session_start();
+
+if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
+    header("location: index.php");
+    exit;
+}
+
+// require_once "php/profile_account.php";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Dashboard</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css" rel="stylesheet" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/fullcalendar@6.1.10/index.global.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@fullcalendar/bootstrap5@6.1.10/index.global.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="./css/dashboard_styles.css" />
+    <link rel="stylesheet" href="./css/profile_page_styles.css" />
+
+
+</head>
+
+<body>
+    <div class="wrapper">
+        <aside id="sidebar" class="shadow-lg">
+            <div class="d-flex">
+                <button id="toggle-btn" type="button">
+                    <i class="bi bi-apple"></i>
+                </button>
+                <div class="sidebar-logo">
+                    <a href="#">TwinPeaks</a>
+                </div>
+            </div>
+            <ul class="sidebar-nav">
+                <li class="sidebar-item">
+                    <a href="dashboard.php" class="sidebar-link">
+                        <i class="bi bi-house"></i>
+                        <span>Home</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a href="#" class="sidebar-link">
+                        <i class="bi bi-calendar2"></i>
+                        <span>Appointments</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a href="#" class="sidebar-link">
+                        <i class="bi bi-envelope"></i>
+                        <span>Notification</span>
+                    </a>
+                </li>
+                <li class="sidebar-footer">
+                    <a href="profile_account_page.php" class="sidebar-link selected">
+                        <i class="bi bi-gear"></i>
+                        <span>Settings</span>
+                    </a>
+                </li>
+            </ul>
+            <div class="sidebar-footer">
+                <a href="php_backend/logout.php" class="sidebar-link">
+                    <i class="bi bi-box-arrow-left"></i>
+                    <span>Logout</span>
+                </a>
+            </div>
+        </aside>
+        <section class="main">
+            <div class="container-fluid" id="settingsArea">
+                <div class="mb-4">
+                    <h1>Settings</h1>
+                </div>
+                <div class="mb-4">
+                    <a href="profile_account_page.php" class="btn chosen">Account</a>
+                    <a href="profile_addresses_page.php" class="btn un-chosen" id="addressButton">Addresses</a>
+                </div>
+                <div class="container-fluid" id="accountSettingArea">
+                    <div>
+                        <h5>Account Settings</h5>
+                    </div>
+                    <form class="row" method="post" novalidate>
+                        <div class="col-md-6 mb-4">
+                            <label class="form-label mb-1">FIRST NAME</label>
+                            <input name="firstName" type="text" class="form-control input-field" placeholder="first Name" aria-label="First name" value="">
+                            <div class="invalid-feedback">
+                                <?php echo $firstName_err; ?>
+                            </div>
+                        </div>
+                        <div class="col-md-6 mb-4">
+                            <label class="form-label mb-1">LAST NAME</label>
+                            <input name="lastName" type="text" class="form-control input-field <?php echo (!empty($lastName_err)) ? 'is-invalid' : ''; ?>" placeholder="last name" aria-label="Last name" value="">
+                            <div class="invalid-feedback">
+                                <?php echo $lastName_err; ?>
+                            </div>
+                        </div>
+                        <div class="mb-4 col-lg-6 mb-4">
+                            <label class="form-label mb-1">EMAIL ADDRESS</label>
+                            <input name="email" type="email" class="form-control fs-6 input-field <?php echo (!empty($emailAddress_err)) ? 'is-invalid' : ''; ?>" placeholder="email address" value="">
+                            <div class="invalid-feedback">
+                                <?php echo $emailAddress_err; ?>
+                            </div>
+                        </div>
+                        <div class="mb-4 col-lg-6 mb-4">
+                            <label class="form-label mb-1">PHONE NUMBER</label>
+                            <input name="email" type="email" class="form-control fs-6 input-field <?php echo (!empty($emailAddress_err)) ? 'is-invalid' : ''; ?>" placeholder="phone number" value="">
+                            <div class="invalid-feedback">
+                                <?php echo $emailAddress_err; ?>
+                            </div>
+                        </div>
+                        <div class="mb-0 col-xl-2">
+                            <button class="btn btn-lg fs-6 w-100 save-changes-button">Save changes</button>
+                        </div>
+                    </form>
+                </div>
+                <div class="container-fluid" id="securitySettingArea">
+                    <div>
+                        <h5>Security</h5>
+                    </div>
+                    <form class="row" method="post" novalidate>
+                        <div class="col-md-6 mb-4">
+                            <label class="form-label mb-1">CURRENT PASSWORD</label>
+                            <input name="firstName" type="text" class="form-control input-field" placeholder="current password" aria-label="Current Password" value="">
+                            <div class="invalid-feedback">
+                                <?php echo $firstName_err; ?>
+                            </div>
+                        </div>
+                        <div class="col-md-6 mb-4">
+                            <label class="form-label mb-1">NEW PASSWORD</label>
+                            <input name="lastName" type="text" class="form-control input-field <?php echo (!empty($lastName_err)) ? 'is-invalid' : ''; ?>" placeholder="new password" aria-label="Last name" value="">
+                            <div class="invalid-feedback">
+                                <?php echo $lastName_err; ?>
+                            </div>
+                        </div>
+                        <div class="mb-4 col-lg-6 mb-4">
+                            <label class="form-label mb-1">CONFIRM NEW PASSWORD</label>
+                            <input name="email" type="email" class="form-control fs-6 input-field <?php echo (!empty($emailAddress_err)) ? 'is-invalid' : ''; ?>" placeholder="confirm new password" value="">
+                            <div class="invalid-feedback">
+                                <?php echo $emailAddress_err; ?>
+                            </div>
+                        </div>
+                    </form>
+                    <div class="mb-0 col-xl-2">
+                        <button class="w-100 btn btn-lg fs-6 change-password-button">Change password</button>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <script src="./js/script.js"></script>
+</body>
+
+</html>
+
+
+
+<!-- Modal -->
+<div class="modal fade" id="updateInfoModal" data-bs-backdrop="static" tabindex="-1" aria-labelledby="updateInfoModal" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" style="width: 500px;">
+        <div class="modal-content shadow p-3 mb-5 bg-white rounded border">
+            <div class="modal-header">
+                <h1 class="modal-title" style="font-size: 20px;" id="exampleModalLabel">Main Details</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+
+                <!-- //TODO: need ug client validation. mo refresh man if server side. prevent pressing save changes if naay invalid input-->
+                <form class="g-4" novalidate id="basicInformationFrm" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+                    <div class="col-md-12 mb-3">
+                        <label class="form-label">First Name</label>
+                        <input name="firstName" type="text" class="form-control input-field <?php echo (!empty($firstName_err)) ? 'is-invalid' : ''; ?>" placeholder="First name" aria-label="First name" value="<?php echo $firstName ?>">
+                        <div class="invalid-feedback">
+                            <?php echo $firstName_err; ?>
+                        </div>
+                    </div>
+                    <div class="col-md-12 mb-3">
+                        <label class="form-label">Last Name</label>
+                        <input name="lastName" type="text" class="form-control input-field <?php echo (!empty($lastName_err)) ? 'is-invalid' : ''; ?>" placeholder="Last name" aria-label="Last name" value="<?php echo $lastName ?>">
+                        <div class="invalid-feedback">
+                            <?php echo $lastName_err; ?>
+                        </div>
+                    </div>
+                    <div class="mb-3 col-lg-12">
+                        <label class="form-label">Email Address</label>
+                        <input name="email" type="email" class="form-control fs-6 input-field <?php echo (!empty($emailAddress_err)) ? 'is-invalid' : ''; ?>" placeholder="Email Address" value="<?php echo $email ?>">
+                        <div class="invalid-feedback">
+                            <?php echo $emailAddress_err; ?>
+                        </div>
+                    </div>
+                    <div class="mb-4 col-lg-12">
+                        <label class="form-label">Phone Number</label>
+                        <input name="mobile" type="text" class="form-control fs-6 input-field <?php echo (!empty($mobileNumber_err)) ? 'is-invalid' : ''; ?>" placeholder="Mobile Number" value="<?php echo $mobileNumber ?>">
+                        <div class="invalid-feedback">
+                            <?php echo $mobileNumber_err; ?>
+                        </div>
+                    </div>
+                    <div class="d-flex justify-content-between">
+                        <button class="btn btn-primary" style="background-color: #124F6F; color: whitesmoke; font-weight: 600;">Save changes</button>
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
