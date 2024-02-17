2fa183f30429dad4abbd0864680f9999b8769cea
Crunchies3 <113993166+Crunchies3@users.noreply.github.com>
2024-02-17 13:33:26 +0800

    added confirmatio kung mag edit

--- client_customer/setting_account_page.php
+++ client_customer/setting_account_page.php
@@ -6,7 +6,7 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
     exit;
 }
 
-require_once "php_backend/profile_account.php";
+require_once "php_backend/settings_account.php";
 
 ?>
 
@@ -16,7 +16,7 @@ require_once "php_backend/profile_account.php";
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Dashboard</title>
+    <title>Settings</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css" rel="stylesheet" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
@@ -61,7 +61,7 @@ require_once "php_backend/profile_account.php";
                     </a>
                 </li>
                 <li class="sidebar-footer">
-                    <a href="profile_page.php" class="sidebar-link selected">
+                    <a href="./setting_account_page.php" class="sidebar-link selected">
                         <i class="bi bi-gear-fill"></i>
                         <span>Settings</span>
                     </a>
@@ -83,7 +83,7 @@ require_once "php_backend/profile_account.php";
                     <div>
                         <h5>Account Settings</h5>
                     </div>
-                    <form class="row" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" novalidate>
+                    <form id="updateAccountDetails" class="row" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" novalidate>
                         <div class="col-md-6 mb-4">
                             <label class="form-label mb-1">FIRST NAME</label>
                             <input name="firstName" type="text" class="form-control input-field" placeholder="Enter your first name" aria-label="Current Password" value="<?php echo $firstName ?>">
@@ -112,13 +112,15 @@ require_once "php_backend/profile_account.php";
                                 <?php echo $emailAddress_err; ?>
                             </div>
                         </div>
+                    </form>
+                    <div class="row">
                         <div class="mb-3 col-xxl-2">
-                            <button name="updateInfo" class="btn btn-lg fs-6 w-100 save-changes-button">Save changes</button>
+                            <button data-bs-toggle="modal" data-bs-target="#updateInfoModal" class="btn btn-lg fs-6 w-100 save-changes-button">Save changes</button>
                         </div>
                         <div class="mb-0 col-xxl-2">
-                            <button name="discardChanges" class="btn btn-lg fs-6 w-100 discard-changes-button">Discard changes</button>
+                            <button form="updateAccountDetails" name="discardChanges" class="btn btn-lg fs-6 w-100 discard-changes-button">Discard changes</button>
                         </div>
-                    </form>
+                    </div>
                 </div>
                 <div class="container-fluid" id="securitySettingArea">
                     <div>
@@ -150,95 +152,17 @@ require_once "php_backend/profile_account.php";
                             </div>
                         </div>
                     </form>
-                    <div class="mb-0 col-xxl-2">
-                        <button name="changePassword" type="submit" form="securityForm" class="btn btn-lg fs-6 w-100 change-password-button">Change password</button>
+                    <div class="row">
+                        <div class="mb-0 col-xxl-2">
+                            <button data-bs-toggle="modal" data-bs-target="#updatePasswordModal" type="submit" class="btn btn-lg fs-6 w-100 change-password-button">Change password</button>
+                        </div>
+                        <div class="mb-0 col-xxl-2">
+                            <button form="updateAccountDetails" name="discardChanges" class="btn btn-lg fs-6 w-100 discard-changes-button">Discard changes</button>
+                        </div>
                     </div>
                 </div>
             </div>
         </section>
-
-
-        <!-- //! DILI NI SIYA WALAON NA COMMENT -->
-        <!-- <section class="main">
-            <div class="container-fluid" id="settingsArea">
-                <div class="mb-4">
-                    <h1>Settings</h1>
-                </div>
-                <div class="mb-4">
-                    <a href="" class="btn chosen">Account</a>
-                    <a href="" class="btn un-chosen" id="addressButton">Addresses</a>
-                </div>
-                <div class="container-fluid" id="accountSettingArea">
-                    <div>
-                        <h5>Account Settings</h5>
-                    </div>
-                    <form class="row" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" novalidate>
-                        <div class="col-md-6 mb-4">
-                            <label class="form-label mb-1">FIRST NAME</label>
-                            <input name="firstName" type="text" class="form-control input-field" placeholder="Enter your first name" aria-label="Current Password" value="<?php echo $firstName ?>">
-                            <div class="invalid-feedback">
-                                <?php echo $firstName_err; ?>
-                            </div>
-                        </div>
-                        <div class="col-md-6 mb-4">
-                            <label class="form-label mb-1">LAST NAME</label>
-                            <input name="lastName" type="text" class="form-control input-field <?php echo (!empty($lastName_err)) ? 'is-invalid' : ''; ?>" placeholder="Enter your last name" aria-label="Last name" value="<?php echo $lastName ?>">
-                            <div class="invalid-feedback">
-                                <?php echo $lastName_err; ?>
-                            </div>
-                        </div>
-                        <div class="mb-4 col-lg-6 mb-4">
-                            <label class="form-label mb-1">EMAIL ADDRESS</label>
-                            <input name="email" type="email" class="form-control fs-6 input-field <?php echo (!empty($emailAddress_err)) ? 'is-invalid' : ''; ?>" placeholder="Enter your email address" value="<?php echo $email ?>">
-                            <div class="invalid-feedback">
-                                <?php echo $emailAddress_err; ?>
-                            </div>
-                        </div>
-                        <div class="mb-4 col-lg-6 mb-4">
-                            <label class="form-label mb-1">PHONE NUMBER</label>
-                            <input name="email" type="email" class="form-control fs-6 input-field <?php echo (!empty($emailAddress_err)) ? 'is-invalid' : ''; ?>" placeholder="Enter your mobile number" value="<?php echo $mobileNumber ?>">
-                            <div class="invalid-feedback">
-                                <?php echo $emailAddress_err; ?>
-                            </div>
-                        </div>
-                        <div class="mb-0 col-xl-2">
-                            <button name="updateInfo" class="btn btn-lg fs-6 w-100 save-changes-button">Save changes</button>
-                        </div>
-                    </form>
-                </div>
-                <div class="container-fluid" id="securitySettingArea">
-                    <div>
-                        <h5>Security</h5>
-                    </div>
-                    <form class="row" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" id="securityForm" novalidate>
-                        <div class="col-md-6 mb-4">
-                            <label class="form-label mb-1">CURRENT PASSWORD</label>
-                            <input name="firstName" type="text" class="form-control input-field" placeholder="Enter your current password" aria-label="Current Password" value="">
-                            <div class="invalid-feedback">
-                                <?php echo $firstName_err; ?>
-                            </div>
-                        </div>
-                        <div class="col-md-6 mb-4">
-                            <label class="form-label mb-1">NEW PASSWORD</label>
-                            <input name="lastName" type="text" class="form-control input-field <?php echo (!empty($lastName_err)) ? 'is-invalid' : ''; ?>" placeholder="Enter your new password" aria-label="Last name" value="">
-                            <div class="invalid-feedback">
-                                <?php echo $lastName_err; ?>
-                            </div>
-                        </div>
-                        <div class="mb-4 col-lg-6 mb-4">
-                            <label class="form-label mb-1">CONFIRM NEW PASSWORD</label>
-                            <input name="email" type="email" class="form-control fs-6 input-field <?php echo (!empty($emailAddress_err)) ? 'is-invalid' : ''; ?>" placeholder="Re-enter your new password" value="">
-                            <div class="invalid-feedback">
-                                <?php echo $emailAddress_err; ?>
-                            </div>
-                        </div>
-                    </form>
-                    <div class="mb-0 col-xl-2">
-                        <button name="changePassword" type="submit" form="securityForm" class="w-100 btn btn-lg fs-6 change-password-button">Change password</button>
-                    </div>
-                </div>
-            </div>
-        </section> -->
         <script src="./js/script.js"></script>
 </body>
 
@@ -246,51 +170,42 @@ require_once "php_backend/profile_account.php";
 
 
 
-<!-- Modal -->
+<!-- //? modal paras confirmation sa pag update sa details -->
+
 <div class="modal fade" id="updateInfoModal" data-bs-backdrop="static" tabindex="-1" aria-labelledby="updateInfoModal" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered" style="width: 500px;">
         <div class="modal-content shadow p-3 mb-5 bg-white rounded border">
             <div class="modal-header">
-                <h1 class="modal-title" style="font-size: 20px;" id="exampleModalLabel">Main Details</h1>
+                <h1 class="modal-title" style="font-size: 20px;" id="exampleModalLabel">Confirm changes?</h1>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
+                Your account details will be updated.
+            </div>
+            <div class="modal-footer">
+                <button name="updateInfo" form="updateAccountDetails" class="btn save-changes-button">Confirm</button>
+                <button type="button" class="btn discard-changes-button" data-bs-dismiss="modal">Cancel</button>
+            </div>
+        </div>
+    </div>
+</div>
 
-                <!-- //TODO: need ug client validation. mo refresh man if server side. prevent pressing save changes if naay invalid input-->
-                <form class="g-4" novalidate id="basicInformationFrm" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-                    <div class="col-md-12 mb-3">
-                        <label class="form-label">First Name</label>
-                        <input name="firstName" type="text" class="form-control input-field <?php echo (!empty($firstName_err)) ? 'is-invalid' : ''; ?>" placeholder="First name" aria-label="First name" value="<?php echo $firstName ?>">
-                        <div class="invalid-feedback">
-                            <?php echo $firstName_err; ?>
-                        </div>
-                    </div>
-                    <div class="col-md-12 mb-3">
-                        <label class="form-label">Last Name</label>
-                        <input name="lastName" type="text" class="form-control input-field <?php echo (!empty($lastName_err)) ? 'is-invalid' : ''; ?>" placeholder="Last name" aria-label="Last name" value="<?php echo $lastName ?>">
-                        <div class="invalid-feedback">
-                            <?php echo $lastName_err; ?>
-                        </div>
-                    </div>
-                    <div class="mb-3 col-lg-12">
-                        <label class="form-label">Email Address</label>
-                        <input name="email" type="email" class="form-control fs-6 input-field <?php echo (!empty($emailAddress_err)) ? 'is-invalid' : ''; ?>" placeholder="Email Address" value="<?php echo $email ?>">
-                        <div class="invalid-feedback">
-                            <?php echo $emailAddress_err; ?>
-                        </div>
-                    </div>
-                    <div class="mb-4 col-lg-12">
-                        <label class="form-label">Phone Number</label>
-                        <input name="mobile" type="text" class="form-control fs-6 input-field <?php echo (!empty($mobileNumber_err)) ? 'is-invalid' : ''; ?>" placeholder="Mobile Number" value="<?php echo $mobileNumber ?>">
-                        <div class="invalid-feedback">
-                            <?php echo $mobileNumber_err; ?>
-                        </div>
-                    </div>
-                    <div class="d-flex justify-content-between">
-                        <button class="btn btn-primary" style="background-color: #124F6F; color: whitesmoke; font-weight: 600;">Save changes</button>
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    </div>
-                </form>
+
+<!-- //? modal paras confirmation sa pag update sa password -->
+
+<div class="modal fade" id="updatePasswordModal" data-bs-backdrop="static" tabindex="-1" aria-labelledby="updatePasswordModal" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" style="width: 500px;">
+        <div class="modal-content shadow p-3 mb-5 bg-white rounded border">
+            <div class="modal-header">
+                <h1 class="modal-title" style="font-size: 20px;" id="exampleModalLabel">Confirm change password?</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                Your password will be updated.
+            </div>
+            <div class="modal-footer">
+                <button name="changePassword" form="securityForm" class="btn save-changes-button">Confirm</button>
+                <button type="button" class="btn discard-changes-button" data-bs-dismiss="modal">Cancel</button>
             </div>
         </div>
     </div>
