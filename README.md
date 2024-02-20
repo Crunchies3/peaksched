af5fdd49f43893904d85426c3799139b91488847
Crunchies3 <113993166+Crunchies3@users.noreply.github.com>
2024-02-20 09:56:33 +0800

    added table function and ui

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 PeakSched: Appointment management system
+
+mga butang na gigamit
+
+-HTML
+-CSS
+-JS
+-BOOTSTRAP5
+-JQUERY
+-JS DATA TABLE
+-JS FULL CALENDAR
+-PHP MAILER
+-GOOGLE SMPT MAIL SERVER
--- client_admin/services_page.php
+++ client_admin/services_page.php
@@ -20,12 +20,17 @@ require_once "php/profile_account.php";
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css" rel="stylesheet" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/fullcalendar@6.1.10/index.global.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@fullcalendar/bootstrap5@6.1.10/index.global.min.js"></script>
+
+    <!-- DataTables CDN -->
+
+    <link href="https://cdn.datatables.net/v/bs5/dt-2.0.0/b-3.0.0/b-html5-3.0.0/r-3.0.0/sl-2.0.0/sr-1.4.0/datatables.min.css" rel="stylesheet">
+    <script src="https://cdn.datatables.net/v/bs5/dt-2.0.0/b-3.0.0/b-html5-3.0.0/r-3.0.0/sl-2.0.0/sr-1.4.0/datatables.min.js"></script>
+    <!-- end -->
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
     <link rel="stylesheet" href="./css/dashboard_styles.css" />
-    <link rel="stylesheet" href="./css/setting_page_styles.css" />
+    <link rel="stylesheet" href="./css/service_page_styles.css" />
 
 
 </head>
@@ -81,44 +86,135 @@ require_once "php/profile_account.php";
             </div>
         </aside>
         <section class="main" id="main">
-            <div class="container-fluid" id="settingsArea">
+            <div class="container-fluid" id="serviceArea">
                 <div class="mb-4">
                     <h1>Services</h1>
                 </div>
-                <div class="container-fluid" id="servicesArea">
-                    <table class="table table-hover">
-                        <thead>
-                            <tr>
-                                <th scope="col">#</th>
-                                <th scope="col">First</th>
-                                <th scope="col">Last</th>
-                                <th scope="col">Handle</th>
-                            </tr>
+                <div class="container-fluid" id="servicesTableArea">
+                    <table id="myTable" class="table table-hover table-borderless">
+                        <!-- //!TODO: para mailisan ang color sa header -->
+                        <thead style="background-color: black !important;">
+                            <th style="color: white;">Description</th>
+                            <th style="color: white;">Actions</th>
                         </thead>
                         <tbody>
                             <tr>
-                                <th scope="row">1</th>
-                                <td>Mark</td>
-                                <td>Otto</td>
-                                <td>@mdo</td>
+                                <td>Regular Cleaning</td>
+                                <td>...</td>
                             </tr>
                             <tr>
-                                <th scope="row">2</th>
-                                <td>Jacob</td>
-                                <td>Thornton</td>
-                                <td>@fat</td>
+                                <td>Detailed Cleaning</td>
+                                <td>...</td>
                             </tr>
                             <tr>
-                                <th scope="row">3</th>
-                                <td colspan="2">Larry the Bird</td>
-                                <td>@twitter</td>
+                                <td>Move-out Cleaning</td>
+                                <td>...</td>
+                            </tr>
+                            <tr>
+                                <td>Air Bnb Cleaning</td>
+                                <td>...</td>
+                            </tr>
+                            <tr>
+                                <td>Carpet Cleaning</td>
+                                <td>...</td>
+                            </tr>
+                            <tr>
+                                <td>Regular Cleaning</td>
+                                <td>...</td>
+                            </tr>
+                            <tr>
+                                <td>Detailed Cleaning</td>
+                                <td>...</td>
+                            </tr>
+                            <tr>
+                                <td>Move-out Cleaning</td>
+                                <td>...</td>
+                            </tr>
+                            <tr>
+                                <td>Air Bnb Cleaning</td>
+                                <td>...</td>
+                            </tr>
+                            <tr>
+                                <td>Carpet Cleaning</td>
+                                <td>...</td>
+                            </tr>
+                            <tr>
+                                <td>Regular Cleaning</td>
+                                <td>...</td>
+                            </tr>
+                            <tr>
+                                <td>Detailed Cleaning</td>
+                                <td>...</td>
+                            </tr>
+                            <tr>
+                                <td>Move-out Cleaning</td>
+                                <td>...</td>
+                            </tr>
+                            <tr>
+                                <td>Air Bnb Cleaning</td>
+                                <td>...</td>
+                            </tr>
+                            <tr>
+                                <td>Carpet Cleaning</td>
+                                <td>...</td>
+                            </tr>
+                            <tr>
+                                <td>Regular Cleaning</td>
+                                <td>...</td>
+                            </tr>
+                            <tr>
+                                <td>Detailed Cleaning</td>
+                                <td>...</td>
+                            </tr>
+                            <tr>
+                                <td>Move-out Cleaning</td>
+                                <td>...</td>
+                            </tr>
+                            <tr>
+                                <td>Air Bnb Cleaning</td>
+                                <td>...</td>
+                            </tr>
+                            <tr>
+                                <td>Carpet Cleaning</td>
+                                <td>...</td>
                             </tr>
                         </tbody>
                     </table>
-
                 </div>
             </div>
         </section>
+
+        <script>
+            $(document).ready(function() {
+                $('#myTable').DataTable();
+            });
+
+            $('#myTable').DataTable({
+                stateSave: true,
+                layout: {
+                    topStart: 'search',
+                    topEnd: 'buttons'
+                },
+                buttons: true,
+                scrollY: 450,
+                responsive: true,
+                language: {
+                    emptyTable: 'No data available in table'
+                },
+                buttons: [{
+                    text: '<i class="bi bi-plus"></i> add service',
+                    action: function(e, dt, node, config) {
+                        let counter = 1;
+                        dt.button().add(1, {
+                            text: 'Button ' + counter++,
+                            action: function() {
+                                this.remove();
+                            }
+                        });
+                    }
+                }]
+            });
+        </script>
         <script src="./js/script.js"></script>
 </body>
 
