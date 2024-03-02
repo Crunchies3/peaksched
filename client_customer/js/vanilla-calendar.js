a2b9bf9426812ef04ee00665a58386bbb6adfb9f
Crunchies3 <113993166+Crunchies3@users.noreply.github.com>
2024-03-02 16:39:48 +0800

    ui paras customer appointment request

---
+++ client_customer/js/vanilla-calendar.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    var today = new Date();
+    var dd = String(today.getDate() + 1).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+    today = yyyy + '-' + mm + '-' + dd;
+
+    const option = {
+        settings: {
+            iso8601: false,
+            visibility: {
+                theme: 'light',
+                weekend: false,
+            },
+            range: {
+                min: today,
+                disableWeekday: [0],
+
+            },
+        },
+        actions: {
+            clickDay(event, self) {
+                alert(self.selectedDates);
+            },
+        }
+    }
+
+    const calendar = new VanillaCalendar('#calendar', option);
+    calendar.init();
+});
--- client_customer/setting_account_page.php
+++ client_customer/setting_account_page.php
@@ -76,7 +76,7 @@ require_once "php_backend/settings_account.php";
         </aside>
         <section class="main" id="main">
             <div class="container-fluid" id="settingsArea">
-                <div class="mb-4">
+                <div class="mb-5">
                     <h1>Settings</h1>
                 </div>
                 <div class="container-fluid" id="accountSettingArea">
