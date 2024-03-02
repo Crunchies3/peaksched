eeb83322b718889c390a170472e1376067e2f0a4
jonaldpaner <jonaldpaner@gmail.com>
2024-03-02 23:08:37 +0800

    ui-assigned-worker

---
+++ client_admin/worker_assigning_page.php
@@ -0,0 +1,162 @@
+<?php
+session_start();
+
+if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
+    header("location: index.php");
+    exit;
+}
+require_once "php/employee_page.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Employee</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css" rel="stylesheet" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+
+    <!-- DataTables CDN -->
+
+    <link href="https://cdn.datatables.net/v/bs5/dt-2.0.0/b-3.0.0/b-html5-3.0.0/r-3.0.0/sl-2.0.0/sr-1.4.0/datatables.min.css" rel="stylesheet">
+    <script src="https://cdn.datatables.net/v/bs5/dt-2.0.0/b-3.0.0/b-html5-3.0.0/r-3.0.0/sl-2.0.0/sr-1.4.0/datatables.min.js"></script>
+    <!-- end -->
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="./css/dashboard_styles.css" />
+    <link rel="stylesheet" href="./css/worker_assigning_styles.css">
+    <link rel="stylesheet" href="../components/_components.css">
+</head>
+
+<body>
+    <div class="wrapper">
+        <aside id="sidebar" class="shadow-lg">
+            <div class="d-flex mb-2">
+                <button id="toggle-btn" type="button">
+                    <i class="bi bi-calendar-week"></i>
+                </button>
+                <div class="sidebar-logo">
+                    <a href="#">PeakSched</a>
+                </div>
+            </div>
+            <ul class="sidebar-nav">
+                <li class="sidebar-item">
+                    <a href="dashboard.php" class="sidebar-link">
+                        <i class="bi bi-house"></i>
+                        <span>Home</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a href="#" class="sidebar-link">
+                        <i class="bi bi-calendar2"></i>
+                        <span>Appointments</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a href="employee_page.php" class="sidebar-link selected">
+                        <i class="bi bi-person-fill"></i>
+                        <span>Employee</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a href="customer_page.php" class="sidebar-link">
+                        <i class="bi bi-emoji-smile"></i>
+                        <span>Customer</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a href="./services_page.php" class="sidebar-link">
+                        <i class="bi bi-file-post-fill"></i>
+                        <span>Services</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a href="#" class="sidebar-link">
+                        <i class="bi bi-bell"></i>
+                        <span>Notifications</span>
+                    </a>
+                </li>
+                <li class="sidebar-footer">
+                    <a href="setting_account_page.php" class="sidebar-link ">
+                        <i class="bi bi-gear"></i>
+                        <span>Settings</span>
+                    </a>
+                </li>
+            </ul>
+            <div class="sidebar-footer">
+                <a href="php/logout.php" class="sidebar-link">
+                    <i class="bi bi-box-arrow-left"></i>
+                    <span>Logout</span>
+                </a>
+            </div>
+        </aside>
+        <section class="main" id="main">
+            <div class="container-fluid" id="assignedWorkerArea">
+                <div class="mb-5">
+                    <h1>Workers</h1>
+                </div>
+                <div class="container-fluid" id="assignedWorkerTableArea">
+                    <div>
+                        <h5>Assign Workers</h5>
+                    </div>
+                    <div class="d-flex justify-content-between mb-3">
+                        <div>Supervisor ID: </div>
+                        <div>Supervisor Name: </div>
+                    </div>
+                    <table id="myTable" class="table table-hover">
+                        <!-- //!TODO: para mailisan ang color sa header -->
+                        <thead id="tableHead">
+                            <th style="color: white;">Id</th>
+                            <th style="color: white;">Fullname</th>
+                            <th style="color: white;">Email</th>
+                            <th style="color: white;">Phone</th>
+                            <th style="color: white;">Actions</th>
+                        </thead>
+                        <tbody>
+                        <?php
+                            // LOOP TILL END OF DATA
+                            while ($rows = $result->fetch_assoc()) {
+                            ?>
+                                <tr>
+                                    <td><?php echo $rows['employeeid']; ?></td>
+                                    <td><?php echo $rows['firstname']; ?> <?php echo $rows['lastname']; ?></td>
+                                    <td><?php echo $rows['email']; ?></td>
+                                    <td><?php echo $rows['mobilenumber']; ?></td>
+                                    <td></td>
+                                </tr>
+                            <?php
+                            }
+                            ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </section>
+        <script src = "./js/data_table_worker_assigning.js"></script>                  
+        <script src="./js/script.js"></script>
+</body>
+
+</html>
+
+
+<div class="modal fade" id="addWorkerModal" data-bs-backdrop="static" tabindex="-1" aria-labelledby="addWorkerModal" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" style="width: 500px;">
+        <div class="modal-content shadow p-3 mb-5 bg-white rounded border">
+            <div class="modal-header">
+                <h1 class="modal-title" style="font-size: 20px;" id="exampleModalLabel">Confirm add worker?</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                Worker will be added.
+            </div>
+            <div class="modal-footer">
+                <button name="addWorkerModal" form="addWorkerForm" class="btn add-service-button">Confirm</button>
+                <button type="button" class="btn cancel-button" data-bs-dismiss="modal">Cancel</button>
+            </div>
+        </div>
+    </div>
+</div>
