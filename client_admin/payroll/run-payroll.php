8fa62edeae26499d22e28d272738f8e12d990535
Crunchies3 <113993166+Crunchies3@users.noreply.github.com>
2024-04-07 13:41:06 +0800

    i validate na ang input kung mag run ug payroll

--- client_admin/payroll/run-payroll.php
+++ client_admin/payroll/run-payroll.php
@@ -120,19 +120,31 @@ require_once "../php/run-payroll.php";
                     <form id="runPayrollForm" class="row" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" novalidate>
                         <div class="col-md-6 mb-4">
                             <label class="form-label mb-1">Start Date</label>
-                            <input type="date" class="form-control input-field" name="startDate" id="startDate">
+                            <input type="date" class="form-control input-field <?php echo (!empty($startDate_err)) ? 'is-invalid' : ''; ?>" name="startDate" id="startDate" value="<?php echo $startDate ?>">
+                            <div class="invalid-feedback">
+                                <?php echo $startDate_err; ?>
+                            </div>
                         </div>
                         <div class="col-md-6 mb-4">
-                            <label class="form-label mb-1">end Date</label>
-                            <input type="date" class="form-control input-field" name="endDate" id="endDate">
+                            <label class="form-label mb-1">End Date</label>
+                            <input type="date" class="form-control input-field <?php echo (!empty($endDateErr)) ? 'is-invalid' : ''; ?>" name="endDate" id="endDate" value="<?php echo $endDate ?>">
+                            <div class="invalid-feedback">
+                                <?php echo $endDateErr; ?>
+                            </div>
                         </div>
                         <div class="col-md-6 mb-4">
                             <label class="form-label mb-1">Federal Tax percent</label>
-                            <input type="text" class="form-control input-field" name="fedTax" id="fedTax">
+                            <input type="text" class="form-control input-field <?php echo (!empty($fedTax_err)) ? 'is-invalid' : ''; ?>" name="fedTax" id="fedTax" value="<?php echo $federalTax ?>">
+                            <div class="invalid-feedback">
+                                <?php echo $fedTax_err; ?>
+                            </div>
                         </div>
                         <div class="col-md-6 mb-4">
                             <label class="form-label mb-1">Pay Date</label>
-                            <input type="date" class="form-control input-field" name="payDate" id="payDate">
+                            <input type="date" class="form-control input-field <?php echo (!empty($paydate_err)) ? 'is-invalid' : ''; ?>" name="payDate" id="payDate" value="<?php echo $payDate ?>">
+                            <div class="invalid-feedback">
+                                <?php echo $paydate_err; ?>
+                            </div>
                         </div>
                     </form>
                     <div class="row">
--- client_admin/php/run-payroll.php
+++ client_admin/php/run-payroll.php
@@ -7,8 +7,12 @@ require_once $_SERVER['DOCUMENT_ROOT'] . "/peaksched/class/payroll.php";
 
 $startDate = $endDate = $payDate = $federalTax = $payrollId = "";
 
+$startDate_err = $endDateErr = $fedTax_err = $paydate_err = "";
+
 $payroll = new Payroll();
 $payroll->setConn($conn);
+
+$validate = new Validation();
 
 
 if ($_SERVER["REQUEST_METHOD"] != "POST") {
@@ -17,12 +21,20 @@ if ($_SERVER["REQUEST_METHOD"] != "POST") {
 
 
 $startDate =  trim($_POST["startDate"]);
+$startDate_err = $validate->PayrollDate($startDate);
+
 $endDate =  trim($_POST["endDate"]);
+$endDateErr = $validate->PayrollDate($endDate);
+
 $payDate =  trim($_POST["payDate"]);
+$paydate_err = $validate->PayrollDate($payDate);
+
 $federalTax =  trim($_POST["fedTax"]);
+$fedTax_err = $validate->PayrollDate($federalTax);
 
 $payrollId = rand(100000, 200000);
 
-
-
-$payroll->runPayroll($payrollId, $startDate, $endDate, $payDate, $federalTax);
+if (empty($startDate_err) && empty($endDateErr) && empty($paydate_err) && empty($fedTax_err)) {
+    $payroll->runPayroll($payrollId, $startDate, $endDate, $payDate, $federalTax);
+    header("location: ./index.php");
+}
