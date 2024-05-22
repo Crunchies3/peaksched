619956ac73e0f89dcbbab1265872fc1dba50c8f6
Crunchies3 <113993166+Crunchies3@users.noreply.github.com>
2024-05-22 19:14:00 +0800

    temp - resched functions

---
+++ client_admin/js/vanilla-calendar.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function () {
+  var today = new Date();
+  var dd = String(today.getDate() + 1).padStart(2, '0');
+  var mm = String(today.getMonth() + 1).padStart(2, '0');
+  var yyyy = today.getFullYear();
+  today = yyyy + '-' + mm + '-' + dd;
+
+  const option = {
+    settings: {
+      iso8601: false,
+      visibility: {
+        theme: 'light',
+        weekend: false,
+      },
+      range: {
+        min: today,
+        disableWeekday: [0],
+      },
+    },
+    actions: {
+      clickDay(event, self) {
+        var element = document.getElementById('selectedDate');
+        element.setAttribute('value', self.selectedDates);
+      },
+    },
+  };
+  const calendar = new VanillaCalendar('#calendar', option);
+  calendar.init();
+});
