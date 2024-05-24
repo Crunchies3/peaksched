037c8942c9688c87fcc6c1c61a697c983c61d244
kixks <kenmentzy@gmail.com>
2024-05-24 21:30:46 +0800

    redirection/style fixes

--- employee/supervisor/reports/report-success.php
+++ employee/supervisor/reports/report-success.php
@@ -21,9 +21,9 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
-    <link rel="stylesheet" href="./css/dashboard_styles.css" />
-    <link rel="stylesheet" href="./css/appointment-styles.css" />
-    <link rel="stylesheet" href="../components/_components.css" />
+    <link rel="stylesheet" href="../../css/dashboard_styles.css" />
+    <link rel="stylesheet" href="../../css/appointment-styles.css" />
+    <link rel="stylesheet" href="../../components/_components.css" />
 
 </head>
 
