543ae6bd2d9e80f8639c4973491294521142e1ce
Crunchies3 <113993166+Crunchies3@users.noreply.github.com>
2025-01-03 22:48:45 +0800

    added new chart paras yearly

--- client_admin/data-analysis/echarts.js
+++ client_admin/data-analysis/echarts.js
@@ -1,8 +1,7 @@
-var chartDom = document.getElementById("demand-per-service")
-var myChart = echarts.init(chartDom)
-var option
-
-option = {
+var demand_per_service = document.getElementById("demand-per-service")
+var myChart_demand_per_service = echarts.init(demand_per_service)
+var option_demand_per_service
+option_demand_per_service = {
     tooltip: {
         trigger: "axis",
         axisPointer: {
@@ -38,4 +37,61 @@ option = {
     ]
 }
 
-option && myChart.setOption(option)
+option_demand_per_service && myChart_demand_per_service.setOption(option_demand_per_service)
+
+// =============================================
+
+var yearly_service = document.getElementById("yearly_service")
+var myChart_yearly_service = echarts.init(yearly_service)
+var option_yearly_service
+
+
+option_yearly_service = {
+    tooltip: {
+        trigger: "axis",
+        axisPointer: {
+            type: "shadow"
+        }
+    },
+    legend: {
+        data: ['Cleaning Service', 'Maintenance Service']
+    },
+    grid: {
+        left: "3%",
+        right: "4%",
+        bottom: "3%",
+        containLabel: true
+    },
+    xAxis: [
+        {
+            type: "category",
+            data: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+            axisTick: {
+                alignWithLabel: true
+            }
+        }
+    ],
+    yAxis: {
+        type: 'value'
+    },
+    series: [
+        {
+            name: "Cleaning Service",
+            data: [60, 95, 93, 100, 118, 110, 113, 83, 60, 64, 48, 48],
+            type: 'line',
+            smooth: true,
+            showSymbol: false
+        },
+        {
+            name: "Maintenance Service",
+            data: [53, 61, 90, 131, 128, 109, 113, 74, 70, 65, 51, 44],
+            type: 'line',
+            smooth: true,
+            showSymbol: false
+        },
+
+    ]
+
+}
+myChart_yearly_service.setOption(option_yearly_service)
+
